5df29316073e3709390f019e956adeb3771773c0
comjoueur <renato.chavez@ucsp.edu.pe>
2017-09-11 17:37:52 -0500

    Add files via upload

---
+++ primos/primos.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+bool testprime(int a,int x=2){
+	if(a<=1) return false;
+	if(x==a) return true;
+	if(a%x==0) return false;
+	return testprime(a,++x);
+}
+int main(){
+	int a;
+	cin>>a;
+	if(testprime(a)) cout<<"es primo"<<endl;
+	else cout<<"no es primo"<<endl;
+}
