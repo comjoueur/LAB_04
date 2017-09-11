b71dd12df5d0c046d7193c37e03480ed07a9bfb8
comjoueur <renato.chavez@ucsp.edu.pe>
2017-09-11 23:50:22 +0000

    Update decrementar_num.cpp

--- Decrementar/decrementar_num.cpp
+++ Decrementar/decrementar_num.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 using namespace std;
 int decrementar(int a,int x=0){//donde x es el iterador
-	if(x==a){return 0;}
-	decrementar(a,x+1);//incrementa el iterador
+	if(x==a){return 0;}//caso base
+	decrementar(a,x+1);//incrementa el iterador,paso recursivo
 	cout<<x<<endl;
 	return 0;
 }
